0443876a78bd34f550ac64e36d641375e4caaa61
Ihsen <bouallegueihsen@gmail.com>
2022-06-06 17:58:38 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Canvas } from "@react-three/fiber";
+
+function App() {
+  return (
+    <div id="canvas-container">
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
+    </div>
+  );
+}
+
+export default App;
