7fd316debe53e127c6291857866051a4afcdbf06
Ihsen <bouallegueihsen@gmail.com>
2022-06-11 11:02:27 +0200

    add user input for 3d parameters

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mantine/core";
+import { Box, NumberInput } from "@mantine/core";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useState } from "react";
@@ -6,6 +6,13 @@ import { TopPart3D } from "./components/TopPart3D";
 
 function App() {
   const [length, setLength] = useState(0);
+  const [topPartParameters, setTopPartParameters] = useState({
+    sigma: 45,
+    height: 20,
+    slantLength: 20,
+    width: 93,
+    totalLength: 700,
+  });
   return (
     <Box id="canvas-container" sx={{ height: "100vh", position: "relative" }}>
       <Canvas>
@@ -16,15 +23,62 @@ function App() {
         <TopPart3D
           position={[0, 0, -length / 2]}
           setLength={setLength}
-          sigma={45}
-          height={20}
-          slantLength={20}
-          width={93}
-          totalLength={700}
+          {...topPartParameters}
         />
         <gridHelper args={[1000, 100]} position={[0, 0, 0]} />
       </Canvas>
-      <Box sx={{ position: "absolute", left: "1em", top: "1em" }}>foo</Box>
+      <Box sx={{ position: "absolute", left: "1em", top: "1em" }}>
+        <NumberInput
+          defaultValue={45}
+          placeholder="Sigma"
+          label="Sigma"
+          required
+          value={topPartParameters.sigma}
+          onChange={(val: number) => {
+            setTopPartParameters({ ...topPartParameters, sigma: val });
+          }}
+        />
+        <NumberInput
+          defaultValue={20}
+          placeholder="Height"
+          label="Height"
+          required
+          value={topPartParameters.height}
+          onChange={(val: number) => {
+            setTopPartParameters({ ...topPartParameters, height: val });
+          }}
+        />
+        <NumberInput
+          defaultValue={20}
+          placeholder="Slant Length"
+          label="Slant Length"
+          required
+          value={topPartParameters.slantLength}
+          onChange={(val: number) => {
+            setTopPartParameters({ ...topPartParameters, slantLength: val });
+          }}
+        />
+        <NumberInput
+          defaultValue={20}
+          placeholder="Width"
+          label="Width"
+          required
+          value={topPartParameters.width}
+          onChange={(val: number) => {
+            setTopPartParameters({ ...topPartParameters, width: val });
+          }}
+        />
+        <NumberInput
+          defaultValue={20}
+          placeholder="Total Length"
+          label="Total Length"
+          required
+          value={topPartParameters.totalLength}
+          onChange={(val: number) => {
+            setTopPartParameters({ ...topPartParameters, totalLength: val });
+          }}
+        />
+      </Box>
     </Box>
   );
 }
--- src/components/TopPart3D.tsx
+++ src/components/TopPart3D.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useLayoutEffect, useRef } from "react";
 import * as THREE from "three";
 import { CalculateTopPart, Vector3toFace } from "../utils/functions";
 
@@ -20,28 +20,26 @@ export const TopPart3D = ({
   totalLength: number;
 }) => {
   const measures = new THREE.Vector3();
-  const ref = useRef<THREE.Mesh>(null!);
-  const { a, b, c, d, e, f, g, h } = CalculateTopPart(
+  const mesh = useRef<THREE.Mesh>(null!);
+  const mirrorMesh = useRef<THREE.Mesh>(null!);
+  let vertices = computeVertices(
     sigma,
     height,
     slantLength,
     width,
     totalLength
   );
-
-  const face1 = Vector3toFace(c, a, b, d);
-  const face2 = Vector3toFace(h, d, b, f);
-  const face3 = Vector3toFace(b, a, e, f);
-  const face4 = Vector3toFace(h, f, e, g);
-  const vertices = new Float32Array([...face1, ...face2, ...face3, ...face4]);
-  useEffect(() => {
-    ref.current.applyMatrix4(new THREE.Matrix4().makeScale(-1, 1, 1));
-    new THREE.Box3().setFromObject(ref.current).getSize(measures);
+  console.log(vertices);
+  useLayoutEffect(() => {
+    mesh.current.geometry.attributes.position.needsUpdate = true;
+    mirrorMesh.current.applyMatrix4(new THREE.Matrix4().makeScale(-1, 1, 1));
+    mirrorMesh.current.geometry.attributes.position.needsUpdate = true;
+    new THREE.Box3().setFromObject(mirrorMesh.current).getSize(measures);
     setLength(measures.z);
-  }, []);
+  }, [sigma, height, slantLength, width, totalLength]);
   return (
     <group position={position}>
-      <mesh>
+      <mesh ref={mesh}>
         <bufferGeometry>
           <bufferAttribute
             attach="attributes-position"
@@ -57,7 +55,7 @@ export const TopPart3D = ({
           metalness={0.5}
         />
       </mesh>
-      <mesh ref={ref}>
+      <mesh ref={mirrorMesh}>
         <bufferGeometry>
           <bufferAttribute
             attach="attributes-position"
@@ -78,3 +76,27 @@ export const TopPart3D = ({
 };
 
 export default TopPart3D;
+
+function computeVertices(
+  sigma: number,
+  height: number,
+  slantLength: number,
+  width: number,
+  totalLength: number
+) {
+  let vertices = new Float32Array();
+  const { a, b, c, d, e, f, g, h } = CalculateTopPart(
+    sigma,
+    height,
+    slantLength,
+    width,
+    totalLength
+  );
+
+  const face1 = Vector3toFace(c, a, b, d);
+  const face2 = Vector3toFace(h, d, b, f);
+  const face3 = Vector3toFace(b, a, e, f);
+  const face4 = Vector3toFace(h, f, e, g);
+  vertices = new Float32Array([...face1, ...face2, ...face3, ...face4]);
+  return vertices;
+}
